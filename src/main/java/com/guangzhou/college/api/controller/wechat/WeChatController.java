88083a10855de7a79cd9e03bfbcb9a5fb6decef0
zhuangjialin <935984143@qq.com>
2020-10-27 18:26:23 +0800

    添加微信获取手机号码

--- src/main/java/com/guangzhou/college/api/controller/wechat/WeChatController.java
+++ src/main/java/com/guangzhou/college/api/controller/wechat/WeChatController.java
@@ -1,10 +1,14 @@
-package com.guangzhou.college.cms.controller.api;
+package com.guangzhou.college.api.controller.wechat;
 
 import com.alibaba.fastjson.JSONObject;
 import com.guangzhou.college.common.ResultInfo;
+import com.guangzhou.college.common.ReturnCodeEnum;
 import com.guangzhou.college.common.utils.AesUtil;
 import com.guangzhou.college.common.utils.HttpUtil;
+import com.guangzhou.college.common.utils.WeChatUtils;
 import com.guangzhou.college.entity.wechat.WxGetUserInfoParam;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,48 +18,63 @@ import java.util.HashMap;
 import java.util.Map;
 
 
-
 @RestController
-@RequestMapping("/wx")
+@Slf4j
+@RequestMapping("api/wechat")
 public class WeChatController {
+
+
+    @Value("${wxConfig.wxspAppid}")
+    private String wxspAppid;
+
+    @Value("${wxConfig.wxspSecret}")
+    private String wxspSecret;
+
+    @Value("${wxConfig.grantType}")
+    private String grantType;
+
+
+
+    @RequestMapping("/getUserPhone")
+    private ResultInfo getUserPhone(@RequestBody WxGetUserInfoParam wxDecrypt){
+        ResultInfo resultInfo = new ResultInfo();
+        WeChatUtils.wxDecrypt(wxDecrypt.getEncryptedData(),wxDecrypt.getSessionKey(),wxDecrypt.getIv());
+        return resultInfo;
+    }
+
+
 
     //@ApiOperation("获取微信小程序的openid")
     //@ResponseBody
     @GetMapping(value = "/getOpenid")
     public ResultInfo getOpenid(String code) throws Exception{
+        ResultInfo resultInfo = new ResultInfo();
 
-        Map<String,Object> map = new HashMap<String,Object>();
         //登录凭证不能为空
         if (code == null || code.length() == 0) {
-            map.put("status", 0);
-            map.put("msg", "code 不能为空");
-            System.out.println("map1:" + map);
             return ResultInfo.newFailResultInfo("code不能为空");
         }
         //小程序唯一标识   (在微信小程序管理后台获取)
-        String wxspAppid = "************";
-        //小程序的 app secret (在微信小程序管理后台获取)
-        String wxspSecret = "************";
-        //授权（必填）  一般都是这个 可不用改
-        String grant_type = "authorization_code";
+//        String wxspAppid = "************";
+//        //小程序的 app secret (在微信小程序管理后台获取)
+//        String wxspSecret = "************";
+//        //授权（必填）  一般都是这个 可不用改
+//        String grant_type = "authorization_code";
         //1、向微信服务器 使用登录凭证 code 获取 session_key 和 openid
         //请求参数
         Map<String,Object> params = new HashMap<>();
         params.put("appid",wxspAppid);
         params.put("secret",wxspSecret);
         params.put("js_code",code);
-        params.put("grant_type",grant_type);
+        params.put("grant_type",grantType);
         //发送请求
         String sr = HttpUtil.get("https://api.weixin.qq.com/sns/jscode2session", params);
         //解析相应内容（转换成json对象）
         JSONObject json = JSONObject.parseObject(sr);
-        //获取会话密钥（session_key）
-        String session_key = json.get("session_key").toString();
-        //用户的唯一标识（openid）
-        String openid = (String) json.get("openid");
-        System.out.println("openid:" + openid);
-
-        return new ResultInfo();
+        log.info("*** json --> {}*** ", json.toJSONString());
+        resultInfo.setData(json);
+        resultInfo.setCode(ReturnCodeEnum.REQUEST_SUCCESS.getStatus());
+        return resultInfo;
     }
 
     //2、对encryptedData加密数据进行AES解密
---
+++ src/main/java/com/guangzhou/college/common/utils/WeChatUtils.java
@@ -0,0 +1,84 @@
+package com.guangzhou.college.common.utils;
+
+import cn.hutool.core.codec.Base64;
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+import javax.crypto.Cipher;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.security.Key;
+import java.security.Security;
+import java.util.Arrays;
+
+public class WeChatUtils {
+
+    /**
+     * 微信小程序获取手机号
+     * @param encryptedData
+     * @param sessionKey
+     * @param iv
+     * @return
+     * @throws Exception
+     */
+    public static JSONObject wxDecrypt (String encryptedData, String sessionKey, String iv) {
+        String result = new String(decryptOfDiyIV(
+                Base64.decode(encryptedData),
+                Base64.decode(sessionKey),
+                Base64.decode(iv)));
+
+
+
+        return JSONUtil.parseObj(result);
+    }
+
+    private static final String KEY_ALGORITHM = "AES";
+    private static final String ALGORITHM_STR = "AES/CBC/PKCS7Padding";
+    private static Key key;
+    private static Cipher cipher;
+
+    private static void init(byte[] keyBytes) {
+        // 如果密钥不足16位，那么就补足.  这个if 中的内容很重要
+        int base = 16;
+        if (keyBytes.length % base != 0) {
+            int groups = keyBytes.length / base + (keyBytes.length % base != 0 ? 1 : 0);
+            byte[] temp = new byte[groups * base];
+            Arrays.fill(temp, (byte) 0);
+            System.arraycopy(keyBytes, 0, temp, 0, keyBytes.length);
+            keyBytes = temp;
+        }
+        // 初始化
+        Security.addProvider(new BouncyCastleProvider());
+        // 转化成JAVA的密钥格式
+        key = new SecretKeySpec(keyBytes, KEY_ALGORITHM);
+        try {
+            // 初始化cipher
+            cipher = Cipher.getInstance(ALGORITHM_STR, "BC");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 解密方法
+     *
+     * @param encryptedData 要解密的字符串
+     * @param keyBytes      解密密钥
+     * @param ivs           自定义对称解密算法初始向量 iv
+     * @return 解密后的字节数组
+     */
+    private static byte[] decryptOfDiyIV(byte[] encryptedData, byte[] keyBytes, byte[] ivs) {
+        byte[] encryptedText = null;
+        init(keyBytes);
+        try {
+            cipher.init(Cipher.DECRYPT_MODE, key, new IvParameterSpec(ivs));
+            encryptedText = cipher.doFinal(encryptedData);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return encryptedText;
+    }
+
+
+}
